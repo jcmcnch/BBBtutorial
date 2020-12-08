af8502ded866501785538f37bcc7a8cb544acab1
Inigo Montoya <inigo@kraken.usc.edu>
2020-12-08 14:42:24 -0800

    add setup scripts

---
+++ setup-scripts/miniconda-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+printf "the hash is:\n1314b90489f154602fd794accfc90446111514a5a72fe1f71ab83e07de9504a7\n"
+sha256sum Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh #say yes to everything
+#rm Miniconda3-latest-Linux-x86_64.sh
